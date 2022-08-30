cdbfd7528fd3fec420035ca1e4850bf76d3509c0
zhLisha <lisazhou1709@gmail.com>
2022-08-30 13:37:37 +0200

    Created Migration

---
+++ database/migrations/2022_08_30_111011_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->text('thumb')->nullable();
+            $table->decimal('price', 5, 2)->unsigner();
+            $table->string('series', 100)->nullable();
+            $table->date('sale_date')->nullable();
+            $table->string('type', 30)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
